c9c21841a799dc7dd741d68d54778265fa3ef1d6
Ravindra Wagh <rwagh@customtravelsolutions.com>
2022-06-29 17:19:11 +0530

    id function updated

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
+function id(n){
+  var add = 1,
+    max = 12 - add;
+
+  if (n > max) {
+    return id(max) + id(n - max);
+  }
+
+  max = Math.pow(10, n + add);
+  var min = max / 10;
+  var number = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  if (("" + number).substring(add).indexOf(0) == 0) {
+    return id(n);
+  }
+
+  return ("" + number).substring(add);
+}
+
 export default {
-  id: (n) => {
-    var add = 1,
-      max = 12 - add;
-
-    if (n > max) {
-      return id(max) + id(n - max);
-    }
-
-    max = Math.pow(10, n + add);
-    var min = max / 10;
-    var number = Math.floor(Math.random() * (max - min + 1)) + min;
-
-    if (("" + number).substring(add).indexOf(0) == 0) {
-      return this.id(n);
-    }
-
-    return ("" + number).substring(add);
-  },
+  id: id,
   capitalize: (value) => {
     let wordArray = value.split(" ");
     let words = [];
